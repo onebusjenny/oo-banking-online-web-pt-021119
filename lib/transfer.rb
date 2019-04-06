c08da2141f4e53fbf981bb2c939331a913c5fc1b
Jenny Chu <jchudesign0930@gmail.com>
2019-04-06 05:49:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
-  attr_accessor :transfer, :sender, :receiver, :status
+  attr_accessor :transfer, :sender, :receiver, :status, :transfer_amount
   
   def initialize(transfer)
     @transfer = transfer
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @transfer_amount = transfer_amount
+end
 end
