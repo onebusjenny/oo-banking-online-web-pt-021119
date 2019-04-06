ad9ba312773b7ce449053e7c72a3aca8439c3e21
Jenny Chu <jchudesign0930@gmail.com>
2019-04-06 05:48:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-
+  attr_accessor :transfer, :sender, :receiver, :status
+  
+  def initialize(transfer)
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
 end
