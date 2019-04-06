c9823e9d8310657ed8c5102a6290efdae2b403de
Jenny Chu <jchudesign0930@gmail.com>
2019-04-06 05:35:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
-
+   attr_accessor :name, :balance, :status
+   
+   def initialize(name)
+     @name = name
+     @balance = 1000
+     @status = "open"
+   end
+   
+   def name=(name)
+     name.freez
+   end
+   
+   def deposit(money)
+     money = 1000
+     @balance << money
+     @balance
+    
+   end
+     
 end
